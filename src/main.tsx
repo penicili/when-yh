3994683a751a78df0b3a51c53cf93b0869072d47
penicili <bagaskoro55@gmail.com>
2026-04-03 15:19:59 +0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import "react-day-picker/style.css";
 import './index.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import { DayPicker } from "react-day-picker";
+import { useState } from "react";
+
 export default function Home() {
+  const [selected, setSelected] = useState<Date | undefined>(undefined);
   return (
-    <></>
-  )
+    <DayPicker
+      animate
+      mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      footer={selected ? <p>Selected date: {selected.toLocaleDateString()}</p> : undefined}
+    />
+  );
 }
